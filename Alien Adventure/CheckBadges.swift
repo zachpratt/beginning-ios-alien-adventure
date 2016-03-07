72184ee31913194e7ae3923668f6844df4c7995c
Zachary Pratt <zachpratt@gmail.com>
2016-03-07 08:47:43 -0600

    Implement special badge animation and check badges

--- Alien Adventure/CheckBadges.swift
+++ Alien Adventure/CheckBadges.swift
@@ -8,8 +8,37 @@
 
 extension Hero {
     
-    func checkBadges(badges: [Badge], requestTypes: [UDRequestType]) -> Bool {
+    func checkBadges(var badges: [Badge], requestTypes: [UDRequestType]) -> Bool {
+        for request in requestTypes {
+            if badges.isEmpty {
+                return false
+            } else {
+                for badge in badges {
+                    let badgeIndex = badges.indexOf(badge)
+                    if badge.requestType.rawValue == request.rawValue {
+                        badges.removeAtIndex(badgeIndex!)
+                    }
+                }
+            }
+        }
         return true
     }
     
 }
+
+
+
+
+//[Alien_Adventure.UDRequestType.BasicCheck, Alien_Adventure.UDRequestType.BasicCheck, Alien_Adventure.UDRequestType.AdvancedCheck, Alien_Adventure.UDRequestType.ExpertCheck]
+
+
+//[<SKSpriteNode> name:'(null)' texture:[<SKTexture> 'BadgeTeal' (275 x 275)] position:{-440, 0} scale:{1.00, 1.00} size:{48, 48} anchor:{0.5, 0.5} rotation:-10.97]
+
+
+
+
+//
+//[<SKSpriteNode> name:'(null)' texture:[<SKTexture> 'BadgeMagenta' (275 x 275)] position:{-370, 0} scale:{1.00, 1.00} size:{48, 48} anchor:{0.5, 0.5} rotation:0.00, <SKSpriteNode> name:'(null)' texture:[<SKTexture> 'BadgeMagenta' (275 x 275)] position:{-300, 0} scale:{1.00, 1.00} size:{48, 48} anchor:{0.5, 0.5} rotation:0.00, <SKSpriteNode> name:'(null)' texture:[<SKTexture> 'BadgeTeal' (275 x 275)] position:{-230, 0} scale:{0.83, 0.83} size:{39.876754760742188, 39.876754760742188} anchor:{0.5, 0.5} rotation:0.00]
+//[<SKSpriteNode> name:'(null)' texture:[<SKTexture> 'BadgeMagenta' (275 x 275)] position:{-300, 0} scale:{1.00, 1.00} size:{48, 48} anchor:{0.5, 0.5} rotation:0.00, <SKSpriteNode> name:'(null)' texture:[<SKTexture> 'BadgeTeal' (275 x 275)] position:{-230, 0} scale:{0.83, 0.83} size:{39.876754760742188, 39.876754760742188} anchor:{0.5, 0.5} rotation:0.00]
+//[<SKSpriteNode> name:'(null)' texture:[<SKTexture> 'BadgeTeal' (275 x 275)] position:{-230, 0} scale:{0.83, 0.83} size:{39.876754760742188, 39.876754760742188} anchor:{0.5, 0.5} rotation:0.00]
+//[]
--- Alien Adventure/SpecialBadge.swift
+++ Alien Adventure/SpecialBadge.swift
@@ -19,41 +19,55 @@ class SpecialBadge: Badge {
         case Rotate = 1
         case Shake = 2
     }
+    
+    func growAndShrink() {
+        let x: Float = 10
+        let y: Float = 6
+        let numberOfTimes = 2.0 / 0.04
+        var actionsArray = [SKAction]()
+        
+        for _ in 1...Int(numberOfTimes) {
+            let dX = Float(arc4random_uniform(UInt32(x))) - x / 2
+            let dY = Float(arc4random_uniform(UInt32(y))) - y / 2
+            let action = SKAction.moveByX(CGFloat(dX), y: CGFloat(dY), duration: 0.02)
+            actionsArray.append(action)
+            actionsArray.append(action.reversedAction())
+        }
+        
+        let sequencedAction = SKAction.sequence(actionsArray)
+        runAction(SKAction.repeatActionForever(sequencedAction))
+    }
+    
+    func rotate() {
+        let action = SKAction.rotateByAngle(CGFloat(-M_PI), duration: 1.5)
+        runAction(SKAction.repeatActionForever(action))
+    }
+    
+    func shake() {
+        let action1 = SKAction.scaleTo(0.8, duration: 1.0)
+        let action2 = SKAction.scaleTo(1.1, duration: 1.0)
+        let sequencedAction = SKAction.sequence([action1, action2])
+        runAction(SKAction.repeatActionForever(sequencedAction))
+    }
+    
+    func animateBadge() {
+        let selectedAnimation = BadgeAnimation(rawValue: Int(arc4random_uniform(3)))
+        
+        switch selectedAnimation! {
+        case BadgeAnimation.GrowAndShrink:
+            growAndShrink()
+        case BadgeAnimation.Rotate:
+            rotate()
+        case BadgeAnimation.Shake:
+            shake()
+        }
+    }
 
     override init(requestType: UDRequestType) {
         super.init(requestType: requestType)
         texture = specialBadgeTexture
-        animateBadge(animation)
+        animateBadge()
         
-    }
-
-    func animateBadge(animationType: Int) {
-        
-        if animation == 0 {
-            let x: Float = 10
-            let y: Float = 6
-            let numberOfTimes = 2.0 / 0.04
-            var actionsArray = [SKAction]()
-            
-            for _ in 1...Int(numberOfTimes) {
-                let dX = Float(arc4random_uniform(UInt32(x))) - x / 2
-                let dY = Float(arc4random_uniform(UInt32(y))) - y / 2
-                let action = SKAction.moveByX(CGFloat(dX), y: CGFloat(dY), duration: 0.02)
-                actionsArray.append(action)
-                actionsArray.append(action.reversedAction())
-            }
-            
-            let sequencedAction = SKAction.sequence(actionsArray)
-            runAction(SKAction.repeatActionForever(sequencedAction))
-        } else if animation == 1 {
-            let action = SKAction.rotateByAngle(CGFloat(-M_PI), duration: 1.5)
-            runAction(SKAction.repeatActionForever(action))
-        } else {
-            let action1 = SKAction.scaleTo(0.8, duration: 1.0)
-            let action2 = SKAction.scaleTo(1.1, duration: 1.0)
-            let sequencedAction = SKAction.sequence([action1, action2])
-            runAction(SKAction.repeatActionForever(sequencedAction))
-        }
     }
     
     
