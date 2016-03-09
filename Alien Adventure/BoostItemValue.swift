0a2579c902161accd1ea49b2deebd455a8c3e509
Zachary Pratt <zachpratt@gmail.com>
2016-03-09 10:25:57 -0600

    Implement Boost Value

--- Alien Adventure/BoostItemValue.swift
+++ Alien Adventure/BoostItemValue.swift
@@ -9,8 +9,6 @@
 extension Hero {
     
     func boostItemValue(inventory: [UDItem]) -> [UDItem] {
-        return [UDItem]()
+        return inventory.map() { UDItem(itemID: $0.itemID, itemType: $0.itemType, name: $0.name, baseValue: $0.baseValue + 100, inscription: $0.inscription, rarity: $0.rarity, historicalData: $0.historicalData) }
     }
 }
-
-// If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 4"
